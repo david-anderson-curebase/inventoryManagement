19767aeef3b748bc026b09accc915895cffba45b
David Anderson <David.Anderson@curebase.com>
2022-06-30 14:26:13 -0700

    method in place for turning Azure CSV into object array

--- inventorySort.js
+++ inventorySort.js
@@ -1,5 +1,24 @@
 var azureInv = require('./azureInventory.json');
 var ws1Inv = require('./ws1Inventory.json');
 
-console.log('Azure Inventory: ' + azureInv.length)
-console.log('WS1 Inventory: ' + ws1Inv.length);
+azObjList = [];
+
+//turn Azure inventory items into objects, insert into azObjList
+const createAzObj = (device) => {
+    const azObj = {
+        deviceName: device.displayName,
+        azureRegistration: device.profileType,
+        id: device.objectId,
+        owner: device.userNames
+    }
+    azObjList.push(azObj);
+};
+
+for (let i = 0; i < azureInv.length; i++) {
+    createAzObj(azureInv[i]);
+};
+
+console.log(azObjList);
+
+/*console.log('Azure Inventory: ' + azureInv)
+console.log('WS1 Inventory: ' + ws1Inv);*/
