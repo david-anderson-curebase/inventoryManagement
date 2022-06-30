e92dd855e9473d3432ed2e85a26412addb4d9f7f
David Anderson <David.Anderson@curebase.com>
2022-06-30 13:58:29 -0700

    first commit. sample files in place

---
+++ inventorySort.js
@@ -0,0 +1,5 @@
+var azureInv = require('./azureInventory.json');
+var ws1Inv = require('./ws1Inventory.json');
+
+console.log('Azure Inventory: ' + azureInv.length)
+console.log('WS1 Inventory: ' + ws1Inv.length);
